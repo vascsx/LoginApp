4207527dc50ba76834a599f13d8ec7efd04f40ca
vascsx <vasczn@gmail.com>
2023-12-20 11:47:34 -0300

    Definindo conexões do SQLite

---
+++ Date/SQLiteData.cs
@@ -0,0 +1,27 @@
+﻿using LoginApp.Model;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginApp.Date
+{
+    public class SQLiteData
+    {
+        readonly SQLiteAsyncConnection _conexaoBD;
+
+
+        // public UsuarioData UsuarioDataTable { get; set; }[
+
+        public SQLiteData(string path)
+        {
+            _conexaoBD = new SQLiteAsyncConnection(path);
+
+            _conexaoBD.CreateTableAsync<Usuario>().Wait();
+
+        }
+
+    }
+}
